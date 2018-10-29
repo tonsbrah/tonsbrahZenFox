0d0aa15d0839e56e8fc9328e3ad8b29db272f14d
tonsbrah <anthony.bagus.p@gmail.com>
2018-10-29 09:59:17 +0700

    update

--- Phase0/week 2/JVweek2.js
+++ Phase0/week 2/JVweek2.js
@@ -1,22 +1,22 @@
 var nama = 'Putra';
-var peran = 'Tabib';
+var peran = '';
 
 if(nama && peran === 'Ksatria'){
     console.log("Selamat datang di Dunia Proxytia, Putra");
     console.log("Halo Ksatria Putra, kamu dapat menyerang dengan senjatamu!");
 }
 else if(nama && peran === 'Tabib') {
-    console.log("Selamat datang di Dunia Proxytia, Danu");
+    console.log("Selamat datang di Dunia Proxytia, Putra");
     console.log("Halo Tabib Putra, kamu akan membantu temanmu yang terluka.");
 }
 
 else if(nama && peran === 'Penyihir') {
-    console.log("Selamat datang di Dunia Proxytia, Zero");
+    console.log("Selamat datang di Dunia Proxytia, Putra");
     console.log("Halo Penyihir Putra, ciptakan keajaiban yang membantu kemenanganmu!");
 }
 
-else if(nama && peran === false){
-    console.log("Halo Mikael, Pilih peranmu untuk memulai game!");
+else if(nama !== true || peran !== false){
+    console.log("Halo Putra, Pilih peranmu untuk memulai game!");
 }
 
 else{
